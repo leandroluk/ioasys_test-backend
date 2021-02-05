5aebcce84e2295db6216eed729aa9fc37c00d2aa
Leandro Santiago Gomes <leandroluk@gmail.com>
2021-02-04 21:51:54 -0300

    chore: add jest, mongodb, bcrypt, jwt and configure tests

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: '@shelf/jest-mongodb',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*.interface.ts', '!<rootDir>/src/**/constants.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+};
