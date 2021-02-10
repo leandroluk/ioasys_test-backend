0b26c68ea1f376122072d02bb9099889f282599d
Leandro Santiago Gomes <leandroluk@gmail.com>
2021-02-10 01:37:48 -0300

    feat: add app and health check base paths

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,11 +2,12 @@ export default {
   app: {
     basePath: process.env.PWD,
     port: process.env.app_port ?? '3000',
-    apiBasePath: process.env.app_apiBasePatb ?? '/api',
-    healthCheckBasePath: process.env.app_healthCheckBasePath ?? '/@/health-check',
+    apiBasePath: process.env.app_apiBasePatb ?? '/@',
+    healthCheckBasePath: process.env.app_healthCheckBasePath ?? '/@/health',
     corsControlAllowOrigin: process.env.app_corsControlAllowOrigin ?? '*'
   },
   mongo: {
+    url: process.env.MONGO_URL ?? process.env.mongo_url ?? 'mongodb://localhost:27017/ioasys_test-backend',
     config: {
       versionCollectionPostfix: process.env.mongo_config_versionCollectionPostfix ?? '_version'
     },
