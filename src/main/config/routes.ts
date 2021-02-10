d7c6f6fdb85b88dae25c33db7b531d9fbb43532d
Leandro Santiago Gomes <leandroluk@gmail.com>
2021-02-10 01:38:23 -0300

    refactor: rework to improve legibility

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,6 +5,6 @@ import env from './env'
 export default (app: Express): void => {
   const router = Router()
   app.use(env.app.apiBasePath, router)
-  fg.sync('**/src/main/routes/**/**route.ts')
-    .map(async file => (await import(`../../../${file}`)).default(router))
+  const routes = fg.sync('**/src/main/routes/**.route.ts')
+  routes.map(async file => (await import(`../../../${file}`)).default(router, app))
 }
