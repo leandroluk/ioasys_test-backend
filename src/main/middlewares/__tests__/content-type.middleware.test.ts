1c64e4526616409fbaf8486428d9ef7579e397be
Leandro Santiago Gomes <leandroluk@gmail.com>
2021-02-09 22:17:13 -0300

    feat: add content-type default response middleware

---
+++ src/main/middlewares/__tests__/content-type.middleware.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest'
+import app from '../../config/app'
+
+describe('contentType', () => {
+  test('should return default content type as json', async () => {
+    const url = '/test_content_type_json'
+
+    app.get(url, (_, res) => res.send(''))
+
+    await request(app)
+      .get(url)
+      .expect('content-type', /json/)
+  })
+
+  test('should return xml content type when forced', async () => {
+    const url = '/test_content_type_xml'
+
+    app.get(url, (_, res) => res.type('xml') && res.send(''))
+
+    await request(app)
+      .get(url)
+      .expect('content-type', /xml/)
+  })
+})
