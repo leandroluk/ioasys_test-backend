55bd839218afd1c01c6e6ad694f775e715e2cd5e
Leandro Santiago Gomes <leandroluk@gmail.com>
2021-02-10 01:39:49 -0300

    refactor: fix problems when change IGetUserByEmail interface

--- src/mocks/domain/use-cases/get-user-by-email.ts
+++ src/mocks/domain/use-cases/get-user-by-email.ts
@@ -1,14 +1,14 @@
-import { IUserWithPasswordModel } from '../../../domain/models/user.interface'
+import { IUserModel, IUserWithPasswordModel } from '../../../domain/models/user.interface'
 import { IGetUserByEmail } from '../../../domain/use-cases/get-user-by-email.interface'
 
 export class GetUserByEmailMock implements IGetUserByEmail {
-  async getUserByEmail(): Promise<IUserWithPasswordModel> {
+  async getUserByEmail<T = IUserModel | IUserWithPasswordModel>(): Promise<T> {
     return await Promise.resolve({
       _id: '_id',
       _created: { at: new Date(), by: 'by' },
       email: 'a@a.com',
       username: 'username',
       password: 'password'
-    })
+    } as any)
   }
 }
