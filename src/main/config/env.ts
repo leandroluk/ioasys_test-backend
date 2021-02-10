35d0813c35f3b8cf182d4349922fb28d68dcdb89
Leandro Santiago Gomes <leandroluk@gmail.com>
2021-02-09 21:56:27 -0300

    feat: add default app configurations to env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,8 @@
 export default {
+  app: {
+    basePath: process.env.PWD,
+    port: process.env.app_port ?? '3000'
+  },
   mongo: {
     config: {
       versionCollectionPostfix: process.env.mongo_config_versionCollectionPostfix ?? '_version'
