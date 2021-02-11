ce2bbb64a80a0e25ef2e766eae7b8b3f441c767b
Leandro Santiago Gomes <leandroluk@gmail.com>
2021-02-10 23:58:52 -0300

    feat: finish tests on sign-up route

--- src/main/routes/__tests__/sign-up.route.test.ts
+++ src/main/routes/__tests__/sign-up.route.test.ts
@@ -1,87 +1,79 @@
-/*
 import faker from 'faker'
 import request from 'supertest'
+import { InvalidParamError } from '../../../errors/invalid-param.error'
+import { MissingParamError } from '../../../errors/missing-param.error'
 import { MongoHelper } from '../../../infra/db/mongodb/helpers/mongo.helper'
 import app from '../../config/app'
 import env from '../../config/env'
-*/
+
 
 describe('sign-up.route', () => {
-  it('should true', () => {})
-  /*
   const url = env.app.apiBasePath + env.routes.signUp
 
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
   beforeEach(async () => await MongoHelper.getCollection(env.mongo.collections.users).deleteMany({}))
   afterAll(async () => await MongoHelper.disconnect())
 
-  describe('ho have body', () => {
-    it('should return 400 if no have body', async () => {
-      await request(app)
-        .post(url)
-        .expect(400)
-        .expect(/Missing param.*body.*?/)
-    })
+  it('should return 400 if no have body', async () => {
+    const response = await request(app).post(url)
+    expect(response.status).toBe(400)
+    expect(JSON.stringify(response)).toMatch(MissingParamError.REGEX_MATCH)
   })
 
-  describe('missing param', () => {
-    it('should return 400 if missing required param', async () => {
-      await request(app)
-        .post(url)
-        .send({ password: 'password' })
-        .expect(400)
-        .expect(/Missing param.*email.*?/)
-      await request(app)
-        .post(url)
-        .send({
-          email: faker.internet.email()
-        })
-        .expect(400)
-        .expect(/Missing param.*password.*?/)
-    })
+  it('should return 400 if missing required param', async () => {
+    await request(app)
+      .post(url)
+      .send({
+        password: 'password'
+      })
+      .expect(400)
+      .expect(MissingParamError.REGEX_MATCH)
+    await request(app)
+      .post(url)
+      .send({
+        email: faker.internet.email()
+      })
+      .expect(400)
+      .expect(MissingParamError.REGEX_MATCH)
   })
 
-  describe('invalid param', () => {
-    it('should return 400 if invalid param', async () => {
-      await request(app)
-        .post(url)
-        .send({
-          email: 'invalid_email',
-          password: 'password'
-        })
-        .expect(400)
-        .expect(/Invalid param/)
-    })
+  it('should return 400 if invalid param', async () => {
+    await request(app)
+      .post(url)
+      .send({
+        email: 'invalid_email',
+        password: 'password'
+      })
+      .expect(400)
+      .expect(InvalidParamError.REGEX_MATCH)
   })
 
-  describe('email already exists', () => {
-    it('should return 400 if email already exists', async () => {
-      const existingUser = {
-        email: 'already@exists.com',
-        password: 'already_exists_com'
-      }
+  it('should return 400 if email already exists', async () => {
+    const existingUser = {
+      email: 'already@exists.com',
+      password: 'already_exists_com'
+    }
 
-      await MongoHelper
-        .getCollection(env.mongo.collections.users)
-        .insertOne(existingUser)
+    await MongoHelper
+      .getCollection(env.mongo.collections.users)
+      .insertOne(existingUser)
 
-      await request(app)
-        .post(url)
-        .send(existingUser)
-        .expect(400)
-    })
+    await request(app)
+      .post(url)
+      .send(existingUser)
+      .expect(400)
   })
 
-  describe('success', () => {
-    it('should return user on success ', async () => {
-      await request(app)
-        .post(url)
-        .send({
-          email: faker.internet.email(),
-          password: faker.internet.password(5)
-        })
-        .expect(200)
-    })
+  it('should return 201 user on success ', async () => {
+    const password = faker.internet.password(5)
+    await request(app)
+      .post(url)
+      .send({
+        email: faker.internet.email(),
+        username: faker.internet.userName(),
+        password,
+        passwordConfirmation: password
+      })
+      .expect(201)
   })
-  */
 })
