4f623cb63ebbdf1a045dd44d49cac1fa3bc5d2f7
Leandro Santiago Gomes <leandroluk@gmail.com>
2021-02-10 23:01:24 -0300

    feat: add userRoles collection env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -15,7 +15,8 @@ export default {
       versionCollectionPostfix: process.env.mongo_config_versionCollectionPostfix ?? '_version'
     },
     collections: {
-      users: process.env.mongo_collection_users ?? 'users'
+      users: process.env.mongo_collection_users ?? 'users',
+      userRoles: process.env.mongo_collection_userRoles ?? 'userRoles'
     }
   },
   routes: {
