4a506bd8383572c05175d6aab805be4e84751861
Leandro Santiago Gomes <leandroluk@gmail.com>
2021-02-10 22:01:49 -0300

    fix: error on SignUpController constructor

--- src/main/factories/sign-up.factory.ts
+++ src/main/factories/sign-up.factory.ts
@@ -1,5 +1,6 @@
 import { SignUpController } from '../../presentation/controllers/sign-up.controller'
 import { AddUserValidator } from '../../presentation/validators/add-user.validator'
+import { NoEmptyBodyValidator } from '../../presentation/validators/no-empty-body.validator'
 import { EmailValidator } from '../../validators/email.validator'
 import { EmptyValidator } from '../../validators/empty.validator'
 import { StringValidator } from '../../validators/string.validator'
@@ -8,7 +9,8 @@ export const makeSignUpController = (): SignUpController => {
   const emptyValidator = new EmptyValidator()
   const emailValidator = new EmailValidator()
   const stringValidator = new StringValidator()
+  const noEmptyBodyValidator = new NoEmptyBodyValidator(emptyValidator)
   const addUserValidator = new AddUserValidator(emptyValidator, emailValidator, stringValidator)
 
-  return new SignUpController(addUserValidator)
+  return new SignUpController(noEmptyBodyValidator, addUserValidator)
 }
